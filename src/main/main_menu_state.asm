e9b22aa8644a4345dd01bb9f2f9acc3e8d62a9fa
Brandon Skari <brandon@skari.org>
2020-05-02 01:24:18 -0600

    Start adding options for traveling the trail
    Need to work on debouncing the keyboard

--- src/main/main_menu_state.asm
+++ src/main/main_menu_state.asm
@@ -2,11 +2,13 @@
 #import "macros.asm"
 .namespace MainMenuState {
 // **** Constants ****
-.const STATE_YOU_MAY = 1
-.const STATE_TRAVEL = 2
-.const STATE_LEARN_ABOUT_GATE_1 = 3
-.const STATE_LEARN_ABOUT_GATE_2 = 4
-.const STATE_LEARN_ABOUT_BURNING_MAN = 5
+.enum {
+    State_YouMay = 1,
+    State_Travel = 2,
+    State_LearnAboutGate1 = 3,
+    State_LearnAboutGate2 = 4,
+    State_LearnAboutBurningMan = 5
+}
 
 _initialize_subroutine_table:
     .word 0  // This space intentionally left blank
@@ -19,19 +21,28 @@ _initialize_subroutine_table:
 _tick_subroutine_table:
     .word 0  // This space intentionally left blank
     .word _tick_you_may
-    .word _tick_space_key_return
+    .word _tick_travel
     .word _tick_space_key_return
     .word _tick_space_key_return
     .word _tick_space_key_return
 
+.var space_to_continue = "press space to continue"
+_space_to_continue:
+.text space_to_continue
+
+.var you_may = "you may:"
+_you_may:
+.text you_may
+
+
 // **** Variables ****
-_state: .byte STATE_YOU_MAY
+_state: .byte State_YouMay
 
 
 // **** Subroutines ****
 
 initialize: {
-    lda #STATE_YOU_MAY
+    lda #State_YouMay
     sta _state
     jsr _initialize_you_may
     rts
@@ -71,7 +82,7 @@ _tick_you_may: {
     lda KEYBOARD_2
     and #%0000_0001
     bne !not_pressed+
-    lda #STATE_TRAVEL
+    lda #State_Travel
     rts
 
 !not_pressed:
@@ -81,7 +92,7 @@ _tick_you_may: {
     lda KEYBOARD_2
     and #%0000_1000
     bne !not_pressed+
-    lda #STATE_LEARN_ABOUT_GATE_1
+    lda #State_LearnAboutGate1
     rts
 
 !not_pressed:
@@ -93,7 +104,45 @@ _tick_you_may: {
     lda KEYBOARD_2
     and #%0000_0001
     bne !not_pressed+
-    lda #STATE_LEARN_ABOUT_BURNING_MAN
+    lda #State_LearnAboutBurningMan
+    rts
+
+!not_pressed:
+    lda #0
+    rts
+}
+
+
+_tick_travel: {
+    // Check for 1 key, row = 7, column = 0
+    lda #%0111_1111
+    sta KEYBOARD_1
+    lda KEYBOARD_2
+    and #%0000_0001
+    bne !not_pressed+
+    lda #State_Travel
+    rts
+
+!not_pressed:
+    // Check for 2 key, row = 7, column = 3
+    lda #%0111_1111
+    sta KEYBOARD_1
+    lda KEYBOARD_2
+    and #%0000_1000
+    bne !not_pressed+
+    lda #State_LearnAboutGate1
+    rts
+
+!not_pressed:
+    // Check for 3 key, row = 1, column = 0
+    // We could put this test right after the check for 1 key, beacuse it
+    // already has column 0 selected, but eeehhhh this is clearer
+    lda #%1111_1101
+    sta KEYBOARD_1
+    lda KEYBOARD_2
+    and #%0000_0001
+    bne !not_pressed+
+    lda #State_LearnAboutBurningMan
     rts
 
 !not_pressed:
@@ -110,7 +159,7 @@ _tick_space_key_return: {
     lda KEYBOARD_2
     and #%0001_0000
     bne !not_pressed+
-    lda #STATE_YOU_MAY
+    lda #State_YouMay
     rts
 
 !not_pressed:
@@ -133,20 +182,17 @@ _call_initialize_subroutine: {
 
 
 _initialize_you_may: {
-    .var greeting = "you may:"
     .var line_1 = "1. enter the gate"
     .var line_2 = "2. learn about the gate"
     .var line_3 = "3. learn about burning man"
 
     jsr clear_screen
-    :draw_string(5, 8, greeting, _greeting)
-    :draw_string(7, 10, line_1, _line_1)
-    :draw_string(7, 11, line_2, _line_2)
-    :draw_string(7, 12, line_3, _line_3)
+    :draw_string(5, 10, you_may, _you_may)
+    :draw_string(7, 12, line_1, _line_1)
+    :draw_string(7, 13, line_2, _line_2)
+    :draw_string(7, 14, line_3, _line_3)
      rts
 
-_greeting:
-    .text greeting
 _line_1:
     .text line_1
 _line_2:
@@ -157,14 +203,47 @@ _line_3:
 
 
 _initialize_travel: {
-    .var greeting = "okay wow let's travel the trail"
+    .var intro_1 = "many kinds of people make the"
+    .var intro_2 = "journey to burning man."
+    .var option_1 = "1. be a billionaire techbro"
+    .var option_2 = "2. be a sparkle pony"
+    .var option_3 = "3. be a veteran burner"
+    .var option_4 = "4. be a bright-eyed bushy-tailed virgin"
+    .var option_5 = "5. found out the differences"
+    .var option_5_2 = "between the choices"
+    .var question = "what is your choice?"
 
     jsr clear_screen
-    :draw_string(5, 8, greeting, _greeting)
-     rts
+    :draw_string(1, 6, intro_1, _intro_1)
+    :draw_string(1, 7, intro_2, _intro_2)
+    :draw_string(1, 9, you_may, _you_may)
+    :draw_string(1, 11, option_1, _option_1)
+    :draw_string(1, 12, option_2, _option_2)
+    :draw_string(1, 13, option_3, _option_3)
+    :draw_string(1, 14, option_4, _option_4)
+    :draw_string(1, 15, option_5, _option_5)
+    :draw_string(4, 16, option_5_2, _option_5_2)
+    :draw_string(1, 18, question, _question)
+    rts
 
-_greeting:
-    .text greeting
+_intro_1:
+    .text intro_1
+_intro_2:
+    .text intro_2
+_option_1:
+    .text option_1
+_option_2:
+    .text option_2
+_option_3:
+    .text option_3
+_option_4:
+    .text option_4
+_option_5:
+    .text option_5
+_option_5_2:
+    .text option_5_2
+_question:
+    .text question
 }
 
 
@@ -173,6 +252,7 @@ _initialize_learn_about_gate_1: {
 
     jsr clear_screen
     :draw_string(5, 8, greeting, _greeting)
+    :draw_string(8, 15, space_to_continue, _space_to_continue)
      rts
 
 _greeting:
@@ -185,6 +265,7 @@ _initialize_learn_about_gate_2: {
 
     jsr clear_screen
     :draw_string(5, 8, greeting, _greeting)
+    :draw_string(8, 15, space_to_continue, _space_to_continue)
      rts
 
 _greeting:
@@ -197,14 +278,11 @@ _initialize_learn_about_burning_man: {
 
     jsr clear_screen
     :draw_string(5, 8, greeting, _greeting)
+    :draw_string(8, 15, space_to_continue, _space_to_continue)
      rts
 
 _greeting:
     .text greeting
 }
-
-.var space_to_continue = "press space to continue"
-_space_to_continue:
-.text space_to_continue
 
 }  // End namespace
